4da84b976f6ddcdc2aad0c9c8e59aa87b0cb4dc5
ciec20151104696 <s20151104696@Mac15.local>
2017-03-08 11:53:46 +0800

    iOS计算器

--- ios/ViewController.swift
+++ ios/ViewController.swift
@@ -10,6 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text: UITextField!
+    @IBAction func add(_ sender: Any) {
+    }
+    
+    @IBAction func down(_ sender: Any) {
+    }
+    
+    @IBAction func multiplication(_ sender: Any) {
+    }
+    @IBAction func division(_ sender: Any) {
+    }
+    
+    @IBAction func equal(_ sender: Any) {
+    }
+    @IBAction func count(_ sender: Any) {
+    }
+    
+    @IBAction func zreo(_ sender: Any) {
+    }
+    @IBAction func one(_ sender: Any) {
+    }
+    @IBAction func two(_ sender: Any) {
+    }
+    @IBAction func three(_ sender: Any) {
+    }
+    @IBAction func four(_ sender: Any) {
+    }
+    @IBAction func five(_ sender: Any) {
+    }
+    @IBAction func six(_ sender: Any) {
+    }
+    @IBAction func seven(_ sender: Any) {
+    }
+    @IBAction func eight(_ sender: Any) {
+    }
+    @IBAction func nine(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
